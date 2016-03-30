119d60b822cebfa04de9819e1ed681b86740af60
weple <weple@qq.com>
2016-03-30 11:09:40 +0800

    Remove useless files

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
-var client = require('./Client/index.js'),
-    server = require('./Server/index.js');
+﻿module.exports = function(app)
+{
+	app.get('/', function(req, res)
+	{
+		res.render('index', {
+			title: '主页',
+			posts: posts
+		});
 
-module.exports = function(app) {
-
-    client(app);
-
-    server(app);
-
-    app.use(function(req, res) {
-        res.render("404");
-    });
-
+	});
 };
