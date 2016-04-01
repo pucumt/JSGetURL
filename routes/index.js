a97f94c97810fd45a0569a881ba6088e6f455a2e
weple <weple@qq.com>
2016-04-01 14:08:32 +0800

    33

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
-﻿var url1 = require('../models/getmanmanbuy.js');
+﻿var url1 = require('../models/getmanmanbuy.js'),
+	Post = require('../../models/post.js'),
+	shortid = require('shortid'),
+	htmlCode = require('../../util/htmlCode.js');
 
 module.exports = function(app)
 {
@@ -10,6 +13,31 @@ module.exports = function(app)
 				title: '主页',
 				posts: posts
 			});
+		});
+	});
+
+	app.post('/post', function(req, res)
+	{
+		var currentUser = req.session.user,
+			post = new Post({
+				shortid: shortid.generate(),
+				title: req.body.title,
+				price: req.body.price,
+				imgFile: req.body.img,
+				linkAddr: req.body.linkAddr,
+				post: htmlCode.htmlEscape(req.body.post),
+				recordBy: "管理员",
+				recordDate: new Date()
+			});
+		post.save(function(err)
+		{
+			if (err)
+			{
+				req.flash('error', err);
+				return res.redirect('/');
+			}
+			req.flash('success', '发布成功!');
+			res.redirect('/'); //发表成功跳转到主页
 		});
 	});
 };
